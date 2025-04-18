d9ecda8fed406330dc97c9d45950f90235f5746c
Song12581 <2606833836@qq.com>
2025-04-18 20:42:16 +0800

    add d/e.cc

---
+++ e.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+
+using std::cout;
+using std::endl;
+
+class Computer{
+public:
+
+private:
+
+};
+
+void test0()
+{
+    cout << " " << endl;
+}
+
+int main()
+{
+    test0();
+    return 0;
+}
+
